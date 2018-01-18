a128db5a2f5cc09fb44105b5a08110f8a68d3b09
Ankur Kedia <ankur@geekyants.com>
2018-01-18 12:46:30 +0530

    Adds screenshots to codes

--- styling/rnstyling/App.js
+++ styling/rnstyling/App.js
@@ -1,22 +1,43 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isLight: true };
+  }
+  toggleTheme = () => {
+    this.setState({ isLight: !this.state.isLight });
+  };
   render() {
     return (
-      <View style={styles.container}>
+      <View
+        style={
+          this.state.isLight ? styles.lightContainer : styles.darkContainer
+        }
+      >
         <Text style={{ fontSize: 32, color: "cyan", fontWeight: "600" }}>
-          This is a sample text
+          Sample text
         </Text>
+        <Button
+          title={this.state.isLight ? "Dark Theme" : "Light Theme"}
+          onPress={this.toggleTheme}
+        />
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  lightContainer: {
     flex: 1,
     backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  darkContainer: {
+    flex: 1,
+    backgroundColor: "#323232",
     alignItems: "center",
     justifyContent: "center"
   }
